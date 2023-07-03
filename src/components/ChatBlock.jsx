dd07b7c945783f46e4c65ccb70a8be6e74ef6ae2
jungwoo3490 <jungwoo3490@naver.com>
2023-07-04 06:44:19 +0900

    피드백 페이지 구현

--- src/components/ChatBlock.jsx
+++ src/components/ChatBlock.jsx
@@ -4,21 +4,21 @@ import chatblock from "../assets/rectangle.png";
 const styles= {
     ic: {
         position: "relative",
-        marginBottom: "100px",
-        left: 0
+        marginBottom: "100px"
     },
     to: {
         position: "absolute",
         top: "20%",
-        left: "40%",
+        left: "38%",
         transform: "translate(-50%, -50%)",
         backgroundColor: "#e4e4e4",
         paddingBottom: "30px",
         color: "#4E5560",
         fontSize: "30px",
-        height: '1px'
+        height: '10px'
     },
     text: {
+        margin: 15,
         //paddingRight: '100px',
         width: '600px', 
         height: '40px'
--- src/pages/ChattingResultPage.jsx
+++ src/pages/ChattingResultPage.jsx
@@ -1,9 +1,30 @@
 import React, { useEffect, useState } from "react";
 import logoImage from "../assets/logo.png";
-import "../styles/ChattingResultPage.css";
+import "../styles/ChattingResultPags.css";
 import { Link } from "react-router-dom";
 import xIcon from "../assets/xIcon.png";
 import checkIcon from "../assets/checkIcon.png";
+import sendIcon from "../assets/sendicon.png";
+const styles = {
+  chatBar: {
+    width: "100%",
+    display: "flex",
+    bottom: 0,
+    position: "absolute",
+    justifyContent: "center",
+    marginBottom: "20px",
+  },
+  in: {
+    width: 1200,
+    height: 70,
+    borderRadius: "20px",
+    fontSize: "30px",
+    paddingLeft: "40px",
+    border: "none",
+    boxShadow: "0 1px 4px 1px rgba(0, 0, 0, 0.2)",
+  },
+}
+
 
 function ChattingResultPage() {
   return (
@@ -11,34 +32,49 @@ function ChattingResultPage() {
       <Link to="/">
         <img src={logoImage} alt="" className="logoimage" />
       </Link>
-      <div className="chatWrap">
-        <div className="wrap">
+      <div className="chatWrap" style={{marginTop: "60px"}}>
+        <div className="wrap" style={{marginBottom: "83px"}}>
           <img
             src={checkIcon}
             alt=""
-            width="30px"
-            height="30px"
-            marginTop="20px"
+            width="61px"
+            height="49px"
+            style={{
+              marginTop: "20px",
+              marginRight: "20px"
+            }}
           ></img>
-          <div className="oChat">hi</div>
+          <div className="oChat" style={{fontSize: "28px", color: "#4e5560", textAlign: "center", lineHeight: "84px"}}>
+            I think he is so kind and he really cares about me.</div>
+        </div>
+        <div style={{width: "631px", height: "32px", fontSize: "28px", marginLeft: "1163px", color: "#730000"}}>이렇게 표현하면 더 좋아요</div>
+        <div className="wrap">
+          <img src={xIcon} alt="" width="52px" height="52px" style={{
+              marginTop: "20px",
+              marginRight: "20px"
+            }}></img>
+          <div className="xChat" style={{fontSize: "28px", color: "#4e5560", textAlign: "center", lineHeight: "84px"}}>I like playing with Princce.</div>
         </div>
         <div className="wrap">
+          <div className="Chat" style={{fontSize: "28px", color: "#4e5560", textAlign: "center", lineHeight: "84px"}}>I like playing with Prince.</div>
+        </div>
+      </div>
+      <div style={styles.chatBar}>
+        <div>
+          <input
+            style={styles.in}
+            type="text"
+            className="chatinput"
+          />
+
+          {/* <button style={styles.btn_style} onClick={handleMessageSend}>
+                    입력
+                </button>                 */}
           <img
-            src={checkIcon}
+            src={sendIcon}
             alt=""
-            width="30px"
-            height="30px"
-            marginTop="20px"
-          ></img>
-          <div className="oChat">hi</div>
-        </div>
-        <div>이렇게 표현하면 더 좋아요</div>
-        <div className="wrap">
-          <img src={xIcon} alt="" width="30px" height="30px"></img>
-          <div className="xChat">hi</div>
-        </div>
-        <div className="wrap">
-          <div className="Chat">hi</div>
+            className="sendicon"
+          />
         </div>
       </div>
     </>
